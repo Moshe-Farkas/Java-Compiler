145fc99c383ae47c7824a1829b76a268368bc9cf
moshe <moishyfarkas9@gmail.com>
2024-02-19 19:00:50 +0200

    Add: new tests

--- src/test/java/com/moshefarkas/javacompiler/semanticanalysis/SymbolTableGenVisitorTest.java
+++ src/test/java/com/moshefarkas/javacompiler/semanticanalysis/SymbolTableGenVisitorTest.java
@@ -1,10 +1,12 @@
 package com.moshefarkas.javacompiler.semanticanalysis;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 import com.moshefarkas.javacompiler.semanticanalysis.SemanticAnalysis.ErrorType;
+import com.moshefarkas.javacompiler.symboltable.Method;
 import com.moshefarkas.javacompiler.symboltable.MethodManager;
 
 public class SymbolTableGenVisitorTest extends BaseSemanticAnalysis {
@@ -43,6 +45,12 @@ public class SymbolTableGenVisitorTest extends BaseSemanticAnalysis {
 
         compileMethodDecl("void sd(int a, float a)");
         assertEquals(ErrorType.DUPLICATE_VAR, visitor.test_error);
+
+        compile("int a; if (true) {int b;} int b;");
+        assertEquals(null, visitor.test_error);
+
+        compile("int a; if (true) {int a;}");
+        assertEquals(ErrorType.DUPLICATE_VAR, visitor.test_error);
     }
 
     @Test 
@@ -50,6 +58,30 @@ public class SymbolTableGenVisitorTest extends BaseSemanticAnalysis {
         // need to compile 
         StringBuilder method = new StringBuilder();
         method.append("public static void mm(int a)");
-        method.append("{int b; int c}");
+        method.append("{int b; int c;}");
+        compileMethod(method.toString());
+        Method m = MethodManager.getInstance().getMethod("mm");
+        m.symbolTable.resetScopes();
+        m.symbolTable.enterScope();
+
+        assertTrue(m.symbolTable.getVarInfo("a").localIndex == 0);
+        assertTrue(m.symbolTable.getVarInfo("b").localIndex == 1);
+        assertTrue(m.symbolTable.getVarInfo("c").localIndex == 2);
+
+        // test with different scopes 
+        method = new StringBuilder();
+        method.append("public static void mm(int a)");
+        method.append("{int b; if (true){int m;} int p;}");
+        compileMethod(method.toString());
+        m = MethodManager.getInstance().getMethod("mm");
+        m.symbolTable.resetScopes();
+        m.symbolTable.enterScope();
+
+        assertTrue(m.symbolTable.getVarInfo("a").localIndex == 0);
+        assertTrue(m.symbolTable.getVarInfo("b").localIndex == 1);
+        m.symbolTable.enterScope();
+        assertTrue(m.symbolTable.getVarInfo("m").localIndex == 2);
+        m.symbolTable.exitScope();
+        assertTrue(m.symbolTable.getVarInfo("p").localIndex == 2);
     }
 }
